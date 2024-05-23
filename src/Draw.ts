64bcc124658284c1a3a1b2ea7e5d534981a7e349
Youry Moise <yourym@mit.edu>
2024-05-22 22:46:55 -0400

    Centered grid

--- src/Draw.ts
+++ src/Draw.ts
@@ -10,16 +10,15 @@ import assert from "assert";
  * @param startX The x coordinate of the top left corner of the square
  * @param startY The y coordinate of the top left corner of the square
  */
-function drawSquare(entry:gridEntry, canvas:HTMLCanvasElement, startX:number, startY:number):void{
+function drawSquare(entry:gridEntry, canvas:HTMLCanvasElement, startX:number, startY:number, sideLength:number):void{
     // planning to have width and height be constant for now
     const context = canvas.getContext('2d');
     assert(context, "No context");
     const totalWidth = canvas.width;
     const totalHeight = canvas.height;
     //wipe the canvas to restart
-    context.clearRect(0,0,totalWidth,totalHeight);
     context.beginPath();
-    context.rect(0,0,100,100);
+    context.rect(startX,startY,100,100);
     context.stroke();
 
 }
@@ -30,5 +29,40 @@ function drawSquare(entry:gridEntry, canvas:HTMLCanvasElement, startX:number, st
  * @param canvas A canvas object to mutate and draw on
  */
 export function drawGrid(grid:Grid,canvas:HTMLCanvasElement):void{
-    drawSquare({letter:'l', color:0}, canvas, 0,0);
+    const screenWidth = window.screen.width;
+    const screenHeight = window.screen.height;
+
+    const padding = 100;
+
+    canvas.width = screenWidth-2*padding;
+    canvas.height = screenHeight-2*padding;
+
+    const context = canvas.getContext('2d');
+    assert(context, "No context");
+    context.clearRect(0,0, canvas.width, canvas.height);
+    const entryList:Array<Array<gridEntry>> = grid.currentState();
+    
+    const numColumns = grid.numColumns;
+    const numRows = grid.numRows;
+    const sideLength = 100;
+
+    const totalGridWidth:number = numColumns*sideLength;
+    const totalGridHeight:number = numRows*sideLength;
+
+    const xOffset = (canvas.width - totalGridWidth)/2;
+    const yOffset = (canvas.height - totalGridHeight)/2;
+
+    let startx = xOffset;
+    let starty = yOffset;
+
+    console.log(entryList);
+    for(const row of entryList){
+        for(const entry of row){
+            drawSquare(entry, canvas, startx, starty, sideLength);
+            console.log("in here", entry);
+            startx+=sideLength;
+        }
+        startx = xOffset;
+        starty += sideLength;
+    }
 }
